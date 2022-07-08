5669aaa02100f753c761fb769f65e84484787c7e
feb-dain <108778921+feb-dain@users.noreply.github.com>
2022-07-08 10:30:16 +0900

    Delete wicked directory

--- wicked/js/halfHeight.js
+++
@@ -1,7 +0,0 @@
-var bw = $(window).width();
-var bh = $(window).height();
-$('#wrap').css({height:bh});
-// 브라우저 높이의 반값 구하기
-var halfHeight = bh/2;
-$('.youtube').css({'margin-top':halfHeight});
-console.log(bw,bh,halfHeight);
