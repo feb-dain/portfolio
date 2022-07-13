49ae6ae207320e8090fe84971f956373e6df43d5
feb-dain <108778921+feb-dain@users.noreply.github.com>
2022-07-13 15:22:39 +0900

    Delete airbnb directory

--- airbnb/js/airbnb_footer.js
+++
@@ -1,9 +0,0 @@
-$('.footer_nav').click(function(){
-    var f_arrow = $(this).next().css('display');
-    $(this).next().stop().toggle();
-    if(f_arrow == 'none'){
-        $(this).children().eq(1).html('<i class="bi bi-chevron-compact-up"></i>');
-    }else{
-        $(this).children().eq(1).html('<i class="bi bi-chevron-compact-down"></i>');
-    }
-});
