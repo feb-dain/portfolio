bf08ea835eb6a85769623f6eb38025395cbd3b04
feb-dain <108778921+feb-dain@users.noreply.github.com>
2022-07-18 14:29:08 +0900

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,13 +15,11 @@ $(document).ready(function() {
     emailjs.init("U7UfuqVy6Mw77-Qk4");
     $('#submit').click(function(){
         var templateParams = {
-        //각 요소는 emailJS에서 설정한 템플릿과 동일한 명으로 작성!
             name: $('#name').val(),
             email : $('#email').val(),
             message : $('#message').val()};
     
         emailjs.send('gmail', 'template_eeuba83', templateParams)
-        //emailjs.send('service ID', 'template ID > emailjs 페이지에서 만든 템플릿에서 확인이 가능하다', 보낼 내용이 담긴 객체)
                 .then(function(response) {
                 alert('감사합니다! 빠른 답장 드리겠습니다.')
                 window.location.reload();
