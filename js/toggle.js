5ef21477340dbed1d63ab50011d20a2bb8723041
feb-dain <108778921+feb-dain@users.noreply.github.com>
2022-07-28 01:54:08 +0900

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -5,22 +5,9 @@ $('.depth1>a').click(function(){
     if($(this).children().hasClass('bi-plus')) {
         $('.tg>a').children().removeClass('bi-dash').addClass('bi-plus');
         $(this).next().stop().slideDown('fast');
-        // $(this).next().css({border:"1px solid #fff"});
         $(this).children().removeClass('bi-plus').addClass('bi-dash');
     } else {
         $(this).next().stop().slideUp('fast');
         $(this).children().removeClass('bi-dash').addClass('bi-plus');
     }
   })
-
-// $('.depth1>a').mouseover(function(){
-//     $('.tg>.depth2').stop().slideUp('fast');
-//     if($(this).children().hasClass('bi-plus')) {
-//         $('.tg>a').children().removeClass('bi-dash').addClass('bi-plus');
-//         $(this).next().stop().slideDown('fast');
-//         $(this).children().removeClass('bi-plus').addClass('bi-dash');
-//     } else {
-//         $(this).next().stop().slideUp('fast');
-//         $(this).children().removeClass('bi-dash').addClass('bi-plus');
-//     }
-// });
