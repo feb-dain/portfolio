d366effc5532594c082fd8c86b399ea3aa3663eb
feb-dain <108778921+feb-dain@users.noreply.github.com>
2022-08-26 13:58:38 +0900

    Delete dktechin directory

--- dktechin/js/calHeight.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    var browserWidth = $(window).width();
-    var browserHeight = $(window).height();
-    console.log('브라우저 너비값 : ' + browserWidth);
-    console.log('브라우저 높이값 : ' + browserHeight);
-
-    $('body').css({height:browserHeight});
-});
